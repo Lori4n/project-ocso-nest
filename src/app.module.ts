b79edbd4876246945437cc20a65e50ccb981e544
Lori4n <norlori4n@gmail.com>
2026-09-13 01:13:19 -0600

    All methods implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { EmployeesModule } from './employees/employees.module.js';
+import { ProductsModule } from './products/products.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+  ConfigModule.forRoot({isGlobal: true}),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.host,
+    port: 5432,
+    username: "postgres",
+    password: process.env.pass,
+    database: process.env.name,
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+  EmployeesModule, 
+  ProductsModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/products/products.service.ts
@@ -0,0 +1,80 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto.js';
+import { UpdateProductDto } from './dto/update-product.dto.js';
+import { v4 as uuid } from 'uuid';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity.js';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>
+  ){}
+
+  private products: CreateProductDto[] = [
+    {
+      productId: uuid(),
+      productName: "Sabritas Normal",
+      price: 29,
+      countSeal: 3,
+      provider: uuid()
+    },
+    {
+      productId: uuid(),
+      productName: "CocaCola 600ml",
+      price: 40,
+      countSeal: 2,
+      provider: uuid()
+    },
+    {
+      productId: uuid(),
+      productName: "Agua Ciel 1L",
+      price: 15,
+      countSeal: 2,
+      provider: uuid()
+    }
+  ]
+  create(createProductDto: CreateProductDto) {
+    const product = this.productRepository.save(createProductDto)
+    return product;
+  }
+
+  findAll() {
+    return this.productRepository.find();
+  }
+
+  findOne(id: string) {
+  const product = this.productRepository.findOneBy({
+    productId: id})
+  if (!product) throw new NotFoundException()
+  return product; 
+  }
+
+  findByProvider(id: string){
+    const productFound = this.products.filter((product) => product.provider == id)
+    if (productFound.length == 0) throw new NotFoundException()
+    return productFound;
+  }
+
+ async update(id: string, updateProductDto: UpdateProductDto) {
+    const productToUpdate = await this.productRepository.preload({
+      productId: id,
+      ...updateProductDto
+    })
+    if (!productToUpdate) throw new NotFoundException()
+    this.productRepository.save(productToUpdate)
+    return productToUpdate;
+  }
+
+  remove(id: string) {
+    this.findOne(id)
+    this.productRepository.delete({
+      productId: id
+    })
+    return {
+      message: "Objeto eliminado correctamente"
+    }
+  }
+}
