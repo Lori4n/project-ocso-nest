07713f021187c6d1bd2e6ac8ed7068a3b38d8f39
Lori4n <norlori4n@gmail.com>
2026-09-25 22:35:32 -0600

    Hasheo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity.js';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>){}
   registerUser(createUserDto: CreateUserDto){
+    createUserDto.userPassword = bcrypt.hashSync(createUserDto.userPassword, 5)
     return this.userRepository.save(createUserDto)
+  }
+  async loginUser(createUserDto: CreateUserDto) {
+    const user = await this.userRepository.findOne({
+      where: {
+        userEmail: createUserDto.userEmail
+      }
+    })
+    if (user == null) throw new BadRequestException
+    const match = await bcrypt.compare(createUserDto.userPassword, user.userPassword)
+    if (!match) throw new UnauthorizedException('No estas autorizado');
+    return;
   }
 }
